6626acb66263583c1143c5cd97be10a1684dec27
aomsumita <sumita_n@ac.th.>
2023-12-11 23:35:03 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,22 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	char ans;
+	if (x == 'A'){
+	    ans = 'Z' ;
+		return ans;
+    }
+    if (x < 'A' || x > 'Z') {
+       ans = '0' ;
+	   return ans ;
+    }
+	else{
+		ans = x-1 ;
+		return ans ;
+	}
 }
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
-}
-
-
-//ให้ใช้ return
